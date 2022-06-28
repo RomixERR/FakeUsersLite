35878876f059ddd7c1a76378d43361366c4ee00d
RomixERR <romixerr@mail.ru>
2022-06-28 21:11:15 +0300

    Добавьте файлы проекта.

---
+++ FakeUserDataset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeUsersLite
+{
+    internal abstract class FakeUserDataset
+    {
+        protected static string NamesFMale = "игорегорвадимойшалексемёниколаропукитемишантолегригоша";
+        protected static string NamesFFemale = "евалисангелиннадеждарьядуняшамашаленаолялякатянкотятка";
+        protected static string NamesL = "петривансидоркошкаптеродактикривкосдураквасякунокжокабалделаскосозуб";
+        protected static string[] NamesLEndsMale = { "ко", "нко","ов", "енков" };
+        protected static string[] NamesLEndsFeMale = { "ко", "нко", "ова","енкова" };
+        protected static string[] NamesMEndsMale = {  "вич"};
+        protected static string[] NamesMEndsFeMale = { "вна" };
+        protected static string NamesM = "игорегорвадимойшалексемёниколартемишантолегригошапетрярикольгоалександрстанислав";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeUsersLite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FakeUser fakeUser = new FakeUser(FakeUser.Egender.Female);
+            for (int i = 0; i < 50; i++)
+            {
+                
+                Console.WriteLine(fakeUser.GetPasport());
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
